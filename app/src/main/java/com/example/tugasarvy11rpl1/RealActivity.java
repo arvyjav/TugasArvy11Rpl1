3f7a0fbb28a0ff0af30c06790c8006b130702af7
arvyjav <arvy.javier.01@gmail.com>
2020-09-04 17:03:29 +0700

    add card view and listdata

--- app/src/main/java/com/example/tugasarvy11rpl1/RealActivity.java
+++ app/src/main/java/com/example/tugasarvy11rpl1/RealActivity.java
@@ -1,14 +1,26 @@
 package com.example.tugasarvy11rpl1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RealActivity extends AppCompatActivity {
-
+    CardView menu1;
+    CardView menu2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_real );
+        menu1 = (CardView)findViewById(R.id.menu1);
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Listdata.class));
+            }
+        });
+        menu2 = (CardView)findViewById(R.id.menu2);
     }
-}
+    }
